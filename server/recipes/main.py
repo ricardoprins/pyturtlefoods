acd61643e2a62e734253b3e513f14584dfad0a90
Ricardo <ricardoprins@gmail.com>
2022-06-06 06:48:43 -0600

    add comment in querystring at api/main.py

--- server/recipes/main.py
+++ server/recipes/main.py
@@ -5,12 +5,13 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-querystring = {
-    "ingredients":"apples,flour,sugar",
-    "number":"5",
-    "ignorePantry":"true",
-    "ranking":"1"
-}
+# sample request format
+#querystring = {
+#    "ingredients":"apples,flour,sugar",
+#    "number":"5",
+#    "ignorePantry":"true",
+#    "ranking":"1"
+#}
 
 @app.get('/query')
 async def get_recipes_by_ingredient(querystring: dict="{}") -> dict:
