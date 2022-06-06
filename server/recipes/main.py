65524f56a4c9c837fc1d1f5feb03ce2958dbe478
Ricardo <ricardoprins@gmail.com>
2022-06-06 06:25:39 -0600

    add basic api structure

---
+++ server/recipes/main.py
@@ -0,0 +1,21 @@
+import os
+import config
+from typing import List
+import requests
+from dotenv import load_dotenv
+from fastapi import FastAPI, Request
+
+
+app = FastAPI()
+
+querystring = {
+    "ingredients":"apples,flour,sugar",
+    "number":"5",
+    "ignorePantry":"true",
+    "ranking":"1"
+}
+
+@app.get('/query')
+async def get_recipes_by_ingredient(querystring: dict) -> dict:
+    response = requests.request("GET", config.URL, headers=config.HEADERS,params=querystring)
+    return(response.text)
